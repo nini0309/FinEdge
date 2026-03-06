543c5e831f359628611a94225eb29103f5a1b4f6
Nishtha Chitalia <nishthachitalia@ip-172-16-15-180.ap-south-1.compute.internal>
2026-03-06 11:33:06 +0530

    edited dockerfile

--- backend/core/database.py
+++ backend/core/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/loanapp"
+#DATABASE_URL = "postgresql://postgres:password@localhost:5432/loanapp"
 
 engine = create_engine(DATABASE_URL)
 
